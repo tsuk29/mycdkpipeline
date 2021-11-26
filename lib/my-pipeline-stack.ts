b61dd5d5edf97e08108632e0f9ac5336a350f8a0
Tomohiro Tsukui <tsukuitt@amazon.co.jp>
2021-11-26 21:11:39 +0900

    Initial commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,35 +1,35 @@
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
-import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
-import { OtherApiStage } from './stages/other-api-stage';
-import { AmplifyStage } from './stages/amplify-stage';
+import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "@aws-cdk/pipelines";
+import { OtherApiStage } from "./stages/other-api-stage";
+import { AmplifyStage } from "./stages/amplify-stage";
 
 /**
  * The stack that defines the application pipeline
  */
- export class MyPipelineStack extends Stack {
+export class MyPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, "Pipeline", {
       // The pipeline name
-      pipelineName: 'MyCdkPipeline',
+      pipelineName: "MyCdkPipeline",
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('<YOUR_GITHUB_USERNAME>/mycdkpipeline', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+      // How it will be built and synthesized
+      synth: new ShellStep("Synth", {
+        // Where the source can be found
+        input: CodePipelineSource.gitHub("tsuk29/mycdkpipeline", "main"),
+
+        // Install dependencies, build and run cdk synth
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
 
     // This is where we add the application stages
-    pipeline.addStage(new AmplifyStage(this, "amplifyStage"))
-    pipeline.addStage(new OtherApiStage(this, "otherApiStage"))
+    pipeline.addStage(new AmplifyStage(this, "amplifyStage"));
+    pipeline.addStage(new OtherApiStage(this, "otherApiStage"));
   }
 }
--- lib/stages/amplify-stage.ts
+++ lib/stages/amplify-stage.ts
@@ -1,13 +1,26 @@
-import { CfnOutput, cfnTagToCloudFormation, Construct, Stage, StageProps } from '@aws-cdk/core';
-// IMPORT AMPLIFY EXPORTED BACKEND HERE
-import * as path from 'path'
-import * as cdk from '@aws-cdk/core'
+import {
+  CfnOutput,
+  cfnTagToCloudFormation,
+  Construct,
+  Stage,
+  StageProps,
+} from "@aws-cdk/core";
+import { AmplifyExportedBackend } from "@aws-amplify/cdk-exported-backend";
+import * as path from "path";
+import * as cdk from "@aws-cdk/core";
 
 export class AmplifyStage extends Stage {
-  
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-    
+
     // ADD AMPLIFY EXPORTED BACKEND STACK HERE
+    const amplifyStack = new AmplifyExportedBackend(
+      this,
+      "amplifyexportedbackend",
+      {
+        path: path.resolve(__dirname, "..", "amplify-export-mytodoapp"),
+        amplifyEnvironment: "dev",
+      }
+    );
   }
 }
